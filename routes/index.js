e85d2d37a17cf73077cf5e53415c476c16edfb76
Mrithul-E <mrithul100@gmail.com>
2026-09-04 23:53:52 +0530

    minor frontend fixes

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const {saveUserLogin} = require("../service/userService")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'InviteHub' });
 });
 
 router.get("/login", (req, res) => {
