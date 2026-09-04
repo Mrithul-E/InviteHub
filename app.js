7e367e5a357e7396efe3be81c5cceb6d1892082a
Mrithul-E <mrithul100@gmail.com>
2026-09-04 14:31:22 +0530

    Add favicon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 var templatesRouter = require('./routes/templates')
@@ -17,6 +18,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
